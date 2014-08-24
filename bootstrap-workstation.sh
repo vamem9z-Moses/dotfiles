f40d332c7cf9acb74486782f67f3bf22087d0ff8
Moses E. Miles III <vamem9z@gmail.com>
2014-08-24 16:17:31 -0400

    Fix config in bootstrap-workstation.sh

--- bootstrap-workstation.sh
+++ bootstrap-workstation.sh
@@ -24,7 +24,7 @@ sudo apt-get remove -y gnumeric abiword
 
 # Install Applications
 echo "Install Apps"
-sudo apt-get install -y  sbcl okular okular-extra-backends aisleriot ctags cmake libpython2.7-dev virtualenvwrapper curl libreadline-dev zsh konsole unrar rar ttf-mscorefonts-installer libreoffice gstreamer0.10-plugins-ugly libxine1-ffmpeg gxine mencoder libdvdread4 totem-mozilla icedax tagtool easytag id3tool lame nautilus-script-audio-convert libmad0 mpg321 libavcodec-extra openjdk-7-jdk vlc clementine wine nautilus-dropbox firewalld firewall-applet chromium-browser filezilla gdebi
+sudo apt-get install -y dkms sbcl okular okular-extra-backends aisleriot ctags cmake libpython2.7-dev virtualenvwrapper curl libreadline-dev zsh konsole unrar rar ttf-mscorefonts-installer libreoffice gstreamer0.10-plugins-ugly libxine1-ffmpeg gxine mencoder libdvdread4 totem-mozilla icedax tagtool easytag id3tool lame nautilus-script-audio-convert libmad0 mpg321 libavcodec-extra openjdk-7-jdk vlc clementine wine nautilus-dropbox firewalld firewall-applet chromium-browser filezilla gdebi
 
 echo "Install CSS"
 sudo /usr/share/doc/libdvdread4/install-css.sh
@@ -37,7 +37,7 @@ echo "Installing rvm"
 curl -sSL https://get.rvm.io | bash -s stable
 
 echo "Install Pythonz"
-sudo apt-get build-dep python2.7
+sudo apt-get -y build-dep python2.7
 curl -kL https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
 
 echo "Install oh-my-zsh"
@@ -48,6 +48,6 @@ echo "Install pysmell"
 sudo pip install pysmell
 
 echo "All of the automated stuff is done"
-echo "Get  .gitconfig and id files and pull down /bin and /dotfiles from github"
+echo "Get  .gitconfig and id files and pull down /bin from github"
 echo "Run bootstrap-thinkpad.sh if your computer is a thinkpad"
 echo "You'll want to install skype, vagrant, virtualbox, google chrome, racket, go, and dart from their websites"
