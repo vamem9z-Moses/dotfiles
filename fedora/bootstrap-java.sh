a940b3ea89948c05a4a2f0a8245c0fbfc8398ab7
Moses E. Miles III <vamem9z@gmail.com>
2015-07-15 17:12:23 -0400

    Refactor Java Dependencies out of main bootstrap script

---
+++ fedora/bootstrap-java.sh
@@ -0,0 +1,4 @@
+#/usr/inb/env bash
+
+echo "Install Java Development Dependencies"
+sudo dnf install -y maven java-1.8.0-openjdk java-1.8.0-openjdk-devel java-1.8.0-openjdk-src java-1.8.0-openjdk-javadoc java-1.8.0-openjdk-headless
