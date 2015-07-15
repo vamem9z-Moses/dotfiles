b378597baf06f0dcbd91461dd82d841cf82fcbed
Moses E. Miles III <vamem9z@gmail.com>
2015-07-15 18:16:35 -0400

    Add eclipse desktop icon creation to bootstrap for downloaded eclipse

--- fedora/bootstrap-java.sh
+++ fedora/bootstrap-java.sh
@@ -2,3 +2,9 @@
 
 echo "Install Java Development Dependencies"
 sudo dnf install -y maven java-1.8.0-openjdk java-1.8.0-openjdk-devel java-1.8.0-openjdk-src java-1.8.0-openjdk-javadoc java-1.8.0-openjdk-headless
+
+echo "Copy Icon to local applications diretory"
+cp ./eclipseicon.xpm $HOME/.local/share/icons
+
+echo "Copy desktop to local application directory"
+cp ./eclipse.desktop $HOME/.local/share/applications
