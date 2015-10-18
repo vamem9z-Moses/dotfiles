f35b18dbf64ec8a350f36cfd31619b087e3740e6
Moses E. Miles III <vamem9z@gmail.com>
2015-10-17 23:18:36 -0400

    Fix broken backup command in bootstrap-lein.sh

--- bootstrap-lein.sh
+++ bootstrap-lein.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Setup lein profiles"
-mv $HOME/lein/profiles.clj $HOME/lein/profiles.clj.bak
+mv $HOME/.lein/profiles.clj $HOME/.lein/profiles.clj.bak
 mkdir $HOME/.lein
 ln -s $PWD/lein/profiles.clj $HOME/.lein/profiles.clj
