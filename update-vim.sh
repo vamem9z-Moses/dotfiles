f1822d245d11c133c0e492eff7c2f397be64c1a2
Moses E. Miles III <vamem9z@gmail.com>
2015-10-17 20:40:15 -0400

    Update YouCompleteMe to use the latest install script

--- update-vim.sh
+++ update-vim.sh
@@ -5,6 +5,6 @@ vim +PluginUpdate +qall
 
 echo "update You YouCompleteMe"
 cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive
-~/.vim/bundle/YouCompleteMe/install.sh
+~/.vim/bundle/YouCompleteMe/install.py
 
 echo "Done"
