07761de9c8a863dde0d6d89cd0d5459f4e97045c
Moses E. Miles III <vamem9z@gmail.com>
2015-10-17 23:02:26 -0400

    Add emacs to system bootstrap

---
+++ bootstrap-emacs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Setup emacs to use spaceemacs"
+
+echo "Backing up old emacs and spaceemacs"
+mv $HOME/.emacs.d $HOME/.emacs.d.bak
+mv $HOME/.spacemacs $/HOME/.spacemacs.old
+
+echo "Setting up new spaceemacs install"
+ln -s $PWD/.spacemacs $HOME/.spacemacs
+git clone --recursive https://github.com/syl20bnr/spacemacs $HOME/.emacs.d
+
+echo "Spacemacs is installed, to complete installation of packages run emacs"
