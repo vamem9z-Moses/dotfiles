4707f09bdf5400a6b1b585dc66ebac6a2026556f
Moses E. Miles III <vamem9z@gmail.com>
2015-11-03 20:56:06 -0500

    Ensure the .emacs file is backed up and not used for spacemacs

--- bootstrap-emacs.sh
+++ bootstrap-emacs.sh
@@ -5,6 +5,7 @@ echo "Setup emacs to use spaceemacs"
 echo "Backing up old emacs and spaceemacs"
 mv $HOME/.emacs.d $HOME/.emacs.d.bak
 mv $HOME/.spacemacs $/HOME/.spacemacs.old
+mv $HOME/.emacs $/HOME/.emacs.old
 
 echo "Setting up new spaceemacs install"
 ln -s $PWD/.spacemacs $HOME/.spacemacs
