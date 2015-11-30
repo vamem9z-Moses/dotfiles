515c33348241b2c608e2190ff42fb3a403771bfa
Moses E. Miles III <vamem9z@gmail.com>
2015-11-30 18:36:36 -0500

    Fix initial YouCompleteMe install

--- bootstrap-vim.sh
+++ bootstrap-vim.sh
@@ -20,6 +20,6 @@ vim +PluginInstall +qall
 
 echo "Install YouCompleteMe"
 cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive
-~/.vim/bundle/YouCompleteMe/install.sh
+~/.vim/bundle/YouCompleteMe/install.py
 
 echo "Done"
