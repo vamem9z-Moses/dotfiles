74750cf332c3c3b8a60b944f507dec5e63f93a2d
Moses E. Miles III <vamem9z@gmail.com>
2016-05-27 18:08:48 -0400

    Update atom configuration
    Add python, html and css support. Add additional file navigation tools.

--- bootstrap-atom.sh
+++ bootstrap-atom.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Install HTML and CSS Packages"
+apm install emmet
+
 echo "Install Go Packages"
 apm install go-plus
 
@@ -19,7 +22,14 @@ echo "Install Code Cleanup Packages"
 apm install atom-beautify
 
 echo "Install General Packages"
-apm install project-manager minimap minimap-pigments split-diff open-recent pigments todo-show
+apm install project-manager open-recent todo-show hyperclick
+
+echo "Install File Navigation Packages"
+apm install synced-sidebar minimap minimap-pigments pigments minimap-split-diff split-diff minimap-highlight-selected highlight-selected
 
 echo "Install Additional JS Support"
 apm install language-babel atom-ternjs
+
+echo "Install Python Support"
+// TODO: Install Virtualenv when support for 1.7.4 is ready
+apm install python-debugger autocomplete-python atom-python-test
