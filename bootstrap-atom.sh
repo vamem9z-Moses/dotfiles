e3a9a21701f4e71241b32e3e05e8ad96aba5ddee
Moses E. Miles III <vamem9z@gmail.com>
2016-07-03 21:05:08 -0400

    Add typescript support

--- bootstrap-atom.sh
+++ bootstrap-atom.sh
@@ -30,6 +30,9 @@ apm install synced-sidebar minimap minimap-pigments pigments minimap-split-diff 
 echo "Install Additional JS Support"
 apm install language-babel atom-ternjs
 
+echo "Install Typscript Support"
+apm install atom-typescript
+
 echo "Install Python Support"
 # TODO: Install Virtualenv when support for 1.7.4 is ready
 apm install python-debugger autocomplete-python atom-python-test
