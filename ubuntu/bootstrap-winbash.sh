6f24f73d2a1d40dfeb500ad96e19b927922ddcb9
Moses E. Miles III <vamem9z@gmail.com>
2017-03-07 20:27:05 -0500

    Fixed software mix, removed unnecessary tools add missing apps

--- ubuntu/bootstrap-winbash.sh
+++ ubuntu/bootstrap-winbash.sh
@@ -9,7 +9,7 @@ sudo apt-get -y upgrade
 # Install Basics
 echo "Install Basics"
 
-sudo apt-get install -y ppa-purge git vim gedit build-essential aptitude lm-sensors htop tree
+sudo apt-get install -y ppa-purge git vim gedit build-essential aptitudehtop tree
 
 # Remove Stuff I don't use
 echo "Remove Apps I don't Use"
@@ -17,7 +17,7 @@ sudo apt-get remove -y gnumeric abiword
 
 # Install Applications
 echo "Install Apps"
-sudo apt-get install -y emacs pylint mercurial dkms sbcl ctags cmake libpython2.7-dev virtualenvwrapper curl libreadline-dev zsh konsole unrar rar
+sudo apt-get install -y pylint mercurial dkms sbcl ctags cmake libpython2.7-dev virtualenvwrapper curl libreadline-dev zsh unrar rar gpgv2
 
 # Install pysmell
 echo "Install pysmell"
@@ -30,16 +30,15 @@ sudo apt-get -y build-dep python2.7
 # Install updated vim for trusty 14.04
 sudo add-apt-repository -y ppa:pi-rho/dev
 sudo apt-get update
-sudo apt-get install vim
+sudo apt-get -y install vim
 
 # Install openjdk8
 sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get update
-sudo apt-get install openjdk-8-jdk
+sudo apt-get -y install openjdk-8-jdk
 
 echo "All of the automated stuff is done"
 echo "Get  .gitconfig and id files and pull down /bin from github"
 echo "Run bootstrap-shell.sh for zsh, rvm, nvm, pythonz and run rvm-pythonz-update.sh
 to make sure you have the latest versions"
 echo "Run bootstrap-vim.sh to setup vim"
-echo "You'll want to install skype, vagrant, virtualbox, google chrome, racket, go, and dart from their websites"
