448d96105ddf8d95f7e623d7b6e54eaf3f1827f7
Moses E. Miles III <vamem9z@gmail.com>
2017-03-19 15:34:56 -0400

    Fix broken apis when using fedora-multimedia
    This limits fedora-multimedia to makemkv, handbrake and libdvdcss. The
    other libraries seemd to cause problems with libraries from rpm fusion.

--- fedora/bootstrap-fedora-hw.sh
+++ fedora/bootstrap-fedora-hw.sh
@@ -13,7 +13,10 @@ echo "Install HW specific utilities"
 sudo dnf install -y powertop tlp smartmontools lm_sensors
 
 echo "Install HW specific apps"
-sudo dnf install -y simple-scan xsane lsdvd dvdbackup HandBrake-gui HandBrake-cli makemkv --allowerasing
+sudo dnf install -y simple-scan xsane lsdvd dvdbackup
+
+echo "Install App from fedora-multimedia"
+sudo dnf install -y HandBrake-gui HandBrake-cli makemkv libdvdcss --allowerasing
 
 echo "Install entertainment apps"
 sudo dnf install -y clementine kodi xpaint minitube vlc sound-juicer aisleriot
@@ -22,7 +25,7 @@ echo "Install virtualization tools"
 sudo dnf install -y virt-manager libvirt qemu* vagrant*
 
 echo "Install Codecs"
-sudo dnf install -y gstreamer-plugins-bad-free-extras gstreamer-plugins-bad-nonfree gstreamer-plugins-bad gstreamer1-plugins-bad-free-extras gstreamer-plugins-ugly gstreamer1-plugins-ugly gstreamer-plugins-good gstreamer1-plugins-good gstreamer-plugins-good-extras gstreamer1-plugins-good-extras gstreamer1-libav gstreamer1-plugins-bad-freeworld gstreamer1-plugins-base-tools gstreamer1-plugins-base gstreamer1 gstreamer-ffmpeg libdvdcss libdvdread libdvdnav libmad lame
+sudo dnf install -y gstreamer1-libav gstreamer1-plugins-bad-nonfree gstreamer-plugins-bad-free-extras gstreamer-plugins-bad-nonfree gstreamer-plugins-bad gstreamer1-plugins-bad-free-extras gstreamer-plugins-ugly gstreamer1-plugins-ugly gstreamer-plugins-good gstreamer1-plugins-good gstreamer-plugins-good-extras gstreamer1-plugins-good-extras gstreamer1-libav gstreamer1-plugins-bad-freeworld gstreamer1-plugins-base-tools gstreamer1-plugins-base gstreamer1 gstreamer-ffmpeg libdvdread libdvdnav libmad lame
 
 sudo dnf install -y gstreamer{1,}-{plugin-crystalhd,ffmpeg,plugins-{good,ugly,bad{,-free,-nonfree,-freeworld,-extras}{,-extras}}} libmpg123 lame-libs --setopt=strict=0
 
