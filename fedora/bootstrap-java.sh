e27f7420d6ee4981a00260db14206e4f9a35d10b
Moses E. Miles III <vamem9z@gmail.com>
2017-12-04 19:25:35 -0500

    Add Java 9 to bootstrap

--- fedora/bootstrap-java.sh
+++ fedora/bootstrap-java.sh
@@ -3,6 +3,9 @@
 echo "Install Java Development Dependencies"
 sudo dnf install -y maven java-1.8.0-openjdk java-1.8.0-openjdk-devel java-1.8.0-openjdk-src java-1.8.0-openjdk-javadoc java-1.8.0-openjdk-headless
 
+echo "Install Java 9 Development Dependencies"
+sudo dnf install java-9* --exclude=*debug*
+
 echo "Copy Icon to local applications diretory"
 mkdir $HOME/.local/share/icons
 cp ./eclipseicon.xpm $HOME/.local/share/icons
