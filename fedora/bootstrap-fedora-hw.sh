53112c3e245a26cb768be42cd50ef122628a87a7
Moses E. Miles III <vamem9z@gmail.com>
2017-12-07 07:00:58 -0500

    Add x264 codec

--- fedora/bootstrap-fedora-hw.sh
+++ fedora/bootstrap-fedora-hw.sh
@@ -28,7 +28,7 @@ echo "Install virtualization tools"
 sudo dnf install -y virt-manager libvirt qemu* vagrant*
 
 echo "Install Codecs"
-sudo dnf install -y gstreamer1-libav gstreamer1-plugins-bad-nonfree gstreamer-plugins-bad-free-extras gstreamer-plugins-bad-nonfree gstreamer-plugins-bad gstreamer1-plugins-bad-free-extras gstreamer-plugins-ugly gstreamer1-plugins-ugly gstreamer-plugins-good gstreamer1-plugins-good gstreamer-plugins-good-extras gstreamer1-plugins-good-extras gstreamer1-libav gstreamer1-plugins-bad-freeworld gstreamer1-plugins-base-tools gstreamer1-plugins-base gstreamer1 gstreamer-ffmpeg libdvdread libdvdnav libmad lame
+sudo dnf install -y gstreamer1-libav gstreamer1-plugins-bad-nonfree gstreamer-plugins-bad-free-extras gstreamer-plugins-bad-nonfree gstreamer-plugins-bad gstreamer1-plugins-bad-free-extras gstreamer-plugins-ugly gstreamer1-plugins-ugly gstreamer-plugins-good gstreamer1-plugins-good gstreamer-plugins-good-extras gstreamer1-plugins-good-extras gstreamer1-libav gstreamer1-plugins-bad-freeworld gstreamer1-plugins-base-tools gstreamer1-plugins-base gstreamer1 gstreamer-ffmpeg libdvdread libdvdnav libmad lame x264
 
 sudo dnf install -y gstreamer{1,}-{plugin-crystalhd,ffmpeg,plugins-{good,ugly,bad{,-free,-nonfree,-freeworld,-extras}{,-extras}}} libmpg123 lame-libs --setopt=strict=0
 
