485b52c93a07f8689382763be63510b506d6021e
Moses E. Miles III <vamem9z@gmail.com>
2017-12-31 23:38:53 -0500

    Add basic setups for cntos servers

---
+++ centos/bootstrap-centos-basic-server.sh
@@ -0,0 +1,21 @@
+echo "Update System"
+sudo yum update -y
+
+echo "Add basic system files"
+sudo yum install -y vim lm_sensors epel-release tree lshw
+
+echo "Update to get latest epel-release"
+sudo yum update -y
+
+echo "Install htop"
+sudo yum install -y htop thermald
+
+echo "Install cockpit"
+sudo yum install cockpit-packagekit cockpit-networkmanager cockpit-storaged cockpit-dashboard cockpit-docker
+
+sudo firewall-cmd --add-service=cockpit
+sudo firewall-cmd --permanent --add-service=cockpit
+sudo firewall-cmd --reload
+
+echo "Running Sensors Detect"
+sudo sensors-detect
