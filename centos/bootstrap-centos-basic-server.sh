24eee1ab4d783c8f7e1a578952d3b3b5b9145484
Moses E. Miles III <vamem9z@gmail.com>
2018-01-01 21:15:33 -0500

    Add ntp and other missing utilities

--- centos/bootstrap-centos-basic-server.sh
+++ centos/bootstrap-centos-basic-server.sh
@@ -1,17 +1,22 @@
 echo "Update System"
 sudo yum update -y
 
-echo "Add basic system files"
-sudo yum install -y vim lm_sensors epel-release tree lshw
+echo "Add basic system tools"
+sudo yum install -y vim lm_sensors epel-release tree lshw pciutils net-tools
 
 echo "Update to get latest epel-release"
 sudo yum update -y
 
 echo "Install htop"
-sudo yum install -y htop thermald
+sudo yum install -y htop thermald ntp
 
 echo "Install cockpit"
 sudo yum install -y cockpit-packagekit cockpit-networkmanager cockpit-storaged cockpit-dashboard cockpit-docker
+
+echo "Setup Time"
+sudo timedatectl set-timezone America/New_York
+sudo systemctl enable ntpd
+sudo systemctl start ntpd
 
 echo "Running Sensors Detect"
 sudo sensors-detect
