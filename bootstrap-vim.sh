072633a38ab9e3a19733e3b527dea7e5409f7629
Moses E. Miles III <vamem9z@gmail.com>
2019-08-04 14:10:14 -0400

    Fix python-mode configuration

--- bootstrap-vim.sh
+++ bootstrap-vim.sh
@@ -18,6 +18,9 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Installing Vim Plugins"
 vim +PluginInstall +qall
 
+echo "Install python-mode"
+cd ~/.vim/bundle/python-mode && git submodule update --init --recursive
+
 echo "Install YouCompleteMe"
 cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive
 ~/.vim/bundle/YouCompleteMe/install.py
--- update-vim.sh
+++ update-vim.sh
@@ -3,7 +3,10 @@
 echo  "Updating Vim Plugins"
 vim +PluginUpdate +qall
 
-echo "update You YouCompleteMe"
+echo "Update python-mode"
+cd ~/.vim/bundle/python-mode && git submodule update --init --recursive
+
+echo "Update YouCompleteMe"
 cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive
 ~/.vim/bundle/YouCompleteMe/install.py
 
