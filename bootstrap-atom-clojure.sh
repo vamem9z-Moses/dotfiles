9ddc9140b9f7c9b4de72211688c89bb0572944b3
Moses E. Miles III <vamem9z@gmail.com>
2019-11-16 09:36:26 -0500

    Add bootstrap for atom clojure

---
+++ bootstrap-atom-clojure.sh
@@ -0,0 +1,14 @@
+echo "Add Parens Support"
+apm install parinfer lisp-paredit rainbow-delimiters
+
+echo "Add Repl Support"
+apm install proto-repl ink
+
+echo "Add Linter Support"
+apm install linter-joker
+
+echo "Add formatter"
+apm install atom-beautify
+
+echo "Install icons"
+apm install atom-file-icons hasklig
