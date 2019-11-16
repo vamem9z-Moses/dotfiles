799c3d95aff2fed16c1da14f1d94406ba2b6c897
Moses E. Miles III <vamem9z@gmail.com>
2019-11-16 12:01:13 -0500

    Remove old unused config from profile.clj

--- profiles.clj
+++
@@ -1,4 +0,0 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]
-                   [refactor-nrepl "1.1.0"]]
-         :dependencies [[alembic "0.3.2"]
-                        [org.clojure/tools.nrepl "0.2.10"]]}}
