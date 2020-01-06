7f8dceac79123256cfc921f3bc0082497136494b
Moses E. Miles III <vamem9z@gmail.com>
2020-01-06 07:05:33 -0500

    Update clojure tools install to fix broken automated install

--- bootstrap-clojure-tools.sh
+++ bootstrap-clojure-tools.sh
@@ -5,7 +5,10 @@ cd $GOPATH/src/github.com/candid82/joker
 ./run.sh --version && go install
 
 echo "Bootstrap node-cljfmt"
-npm install -g node-cljfmt
+sudo npm install -g node-cljfmt
 
 echo "Bootstrap clj-kondo"
-npm install -g clj-kondo
+cd $HOME/bin
+wget https://raw.githubusercontent.com/borkdude/clj-kondo/master/script/install-clj-kondo 
+bash install-clj-kondo --dir $HOME/bin
+rm install-clj-kondo
