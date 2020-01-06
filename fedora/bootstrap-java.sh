4ee573b67c7440882ce37a0e3f08d2013db5e57c
Moses E. Miles III <vamem9z@gmail.com>
2020-01-05 23:22:11 -0500

    Update java bootstrap to jdk 11

--- fedora/bootstrap-java.sh
+++ fedora/bootstrap-java.sh
@@ -1,16 +1,4 @@
 #/usr/inb/env bash
 
 echo "Install Java Development Dependencies"
-sudo dnf install -y maven java-1.8.0-openjdk java-1.8.0-openjdk-devel java-1.8.0-openjdk-src java-1.8.0-openjdk-javadoc java-1.8.0-openjdk-headless
-
-echo "Install Java 9 Development Dependencies"
-sudo dnf install java-9* --exclude=*debug*
-
-echo "Copy Icon to local applications diretory"
-mkdir $HOME/.local/share/icons
-cp ./eclipseicon.xpm $HOME/.local/share/icons
-
-echo "Copy desktop to local application directory"
-cp ./eclipse.desktop $HOME/.local/share/applications
-
-echo "Be sure to create a symbolic link between the eclipse executable and the $/HOME/bin directory e.g. ln -s ~/Apps/eclipse/eclipse ~/bin"
+sudo dnf install -y maven java-11-openjdk java-11-openjdk-devel java-11-openjdk-src java-11-openjdk-javadoc java-11-openjdk-headless
