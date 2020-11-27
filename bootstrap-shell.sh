49fb746043a3a9b4b6bf0c98c3dca95e2b7f66ca
Moses E. Miles III <vamem9z@gmail.com>
2020-11-27 08:47:28 -0500

    Update rvm gpg key

--- bootstrap-shell.sh
+++ bootstrap-shell.sh
@@ -6,7 +6,7 @@ echo "Installing nvm"
 curl https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash
 
 echo "Installing rvm"
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
 
 echo "Install Pythonz"
