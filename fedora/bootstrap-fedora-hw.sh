9a9567357c640555aeae59949fd16230500fe189
Moses E. Miles III <vamem9z@gmail.com>
2021-01-16 19:42:09 -0500

    Update hw bootstrap for fedora 33

--- fedora/bootstrap-fedora-hw.sh
+++ fedora/bootstrap-fedora-hw.sh
@@ -22,20 +22,17 @@ echo "Install entertainment apps"
 sudo dnf install -y clementine kodi vlc sound-juicer aisleriot
 
 echo "Install dropbox"
-sudo dnf install -y nautilus-dropbox nemo-dropbox dropbox
+sudo dnf install -y nautilus-dropbox dropbox
 
 echo "Install virtualization tools"
 sudo dnf install -y virt-manager libvirt qemu* vagrant*
 
 echo "Install Codecs"
-sudo dnf install -y gstreamer1-libav gstreamer1-plugins-bad-nonfree gstreamer-plugins-bad-free-extras gstreamer-plugins-bad-nonfree gstreamer-plugins-bad gstreamer1-plugins-bad-free-extras gstreamer-plugins-ugly gstreamer1-plugins-ugly gstreamer-plugins-good gstreamer1-plugins-good gstreamer-plugins-good-extras gstreamer1-plugins-good-extras gstreamer1-libav gstreamer1-plugins-bad-freeworld gstreamer1-plugins-base-tools gstreamer1-plugins-base gstreamer1 gstreamer-ffmpeg libdvdread libdvdnav libmad lame x264
+sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
 
-sudo dnf install -y gstreamer{1,}-{plugin-crystalhd,ffmpeg,plugins-{good,ugly,bad{,-free,-nonfree,-freeworld,-extras}{,-extras}}} libmpg123 lame-libs --setopt=strict=0
+sudo dnf install -y lame\* --exclude=lame-devel
 
-echo "Use Fedora mpeq mp3 support"
-sudo dnf remove -y gstreamer1-plugins-ugly
-
-sudo dnf install -y gstreamer1-plugin-mpg123 mpg123-libs
+sudo dnf group upgrade -y --with-optional Multimedia
 
 # Setup thermald
 sudo dnf install -y thermald
