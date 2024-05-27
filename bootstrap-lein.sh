300aeec88a8d01b5897b0da891fc35b04ff1cf5c
Moses E. Miles III <vamem9z@gmail.com>
2024-05-27 14:15:01 +0000

    Remove unused lein config

--- bootstrap-lein.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Run lein for first time"
-lein
-echo "Setup lein profiles"
-mv $HOME/.lein/profiles.clj $HOME/.lein/profiles.clj.bak
-mkdir $HOME/.lein
-ln -s $PWD/lein/profiles.clj $HOME/.lein/profiles.clj
