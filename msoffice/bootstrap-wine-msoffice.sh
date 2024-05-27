9c771746da10c80947a2ab621d476b18aecafab9
Moses E. Miles III <vamem9z@gmail.com>
2024-05-27 14:04:26 +0000

    Remove unused configs

--- msoffice/bootstrap-wine-msoffice.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Copy icon files for desktop entries"
-cp -R Office07 $HOME/.local/share/icons
-
-echo "Copy desktop entries for MS Office"
-cp desktop/*.desktop $HOME/.local/share/applications
