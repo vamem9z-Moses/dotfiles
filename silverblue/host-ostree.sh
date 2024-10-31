f09fa04477368c5f25f2c7c53dcf20df8f48d1c8
Moses E. Miles III <vamem9z@gmail.com>
2024-10-31 16:29:49 -0400

    update silverblue config

--- silverblue/host-ostree.sh
+++ silverblue/host-ostree.sh
@@ -3,7 +3,7 @@
 echo "Install apps into the System OS Tree"
 echo "" 
 
-sudo rpm-ostree install distrobox htop tilix zsh gnome-tweaks lm_sensors powertop podman-compose
+sudo rpm-ostree install distrobox htop tilix zsh gnome-tweaks lm_sensors powertop podman-compose nautilus-gsconnect gnome-shell-extension-gsconnect
 
 echo "Set inotify watchers to higher limit for distrobox"
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
@@ -11,3 +11,6 @@ echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo s
 echo "" 
 echo "" 
 echo "Reboot to load the new image"
+echo "" 
+echo "When setting up terminal profiles be sure to pass a specifc terminal to tmux"
+echo "Example: distrobox enter -n apps -- tmux -L apps"
