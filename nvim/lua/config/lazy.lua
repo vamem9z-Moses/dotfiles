cd253957225962e5337a41e2e0785c4919f847d7
Moses E. Miles III <vamem9z@gmail.com>
2024-12-16 21:40:17 -0500

    Update nvim setup

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -51,3 +51,4 @@ require("lazy").setup({
     },
   },
 })
+
--- silverblue/distrobox-debian-dev.sh
+++ silverblue/distrobox-debian-dev.sh
@@ -8,11 +8,11 @@ sudo apt -y update && sudo apt -y upgrade
 echo "Install utilities"
 sudo apt -y install zsh git tmux vim python-is-python3 gnome-keyring wget gpg fontconfig openssl software-properties-common gettext make cmake
 
-# Add Development Tools 
+# Add Development Tools
 echo "Install VSCode"
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null
 rm -f packages.microsoft.gpg
 sudo apt -y install apt-transport-https
 sudo apt update
@@ -20,7 +20,7 @@ sudo apt -y install code # or code-insiders
 
 ## Add dependencies to build neovim from source
 echo "Add neovim build dependencies"
-sudo apt install -y gettext make cmake
+sudo apt install -y gettext make cmake fzf
 
 ## Add dependencies to build ruby
 echo "Add ruby build dependencies"
@@ -34,12 +34,12 @@ sudo apt -y install build-essential zlib1g-dev libbz2-dev libssl-dev libreadline
 echo "Add clojure dependencies"
 sudo apt -y install rlwrap
 
-
 # Podman
 echo "Make Podman and Podman-Compose available to apps container"
 sudo ln -s /usr/bin/distrobox-host-exec /usr/bin/podman
 sudo ln -s /usr/bin/distrobox-host-exec /usr/bin/podman-compose
 
-# Fonts 
+# Fonts
 echo "Make fonts available inside of the container"
 fc-cache -f -v
+
