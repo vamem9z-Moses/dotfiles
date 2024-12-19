6501a3d3e2dac350f417e58040773a8e1b0a29c4
Moses E. Miles III <vamem9z@gmail.com>
2024-12-19 18:54:09 -0500

    Remove use NetworkManager with UI infavor of static IP address

--- debian/bootstrap-debian-gnome-minimal.sh
+++ debian/bootstrap-debian-gnome-minimal.sh
@@ -15,23 +15,6 @@ sudo nala -y install lightdm
 echo "Setup graphical boot"
 sudo systemctl enable --now lightdm && sudo systemctl set-default graphical.target
 
-echo "Update /etc/NetworkManager/NetworkManager.conf and changes managed=false to managed=true and then reboot 'sudo reboot'"
-
-# conf file
-network_manager_config_file = "/etc/NetworkManager/NetworkManager.conf"
-
-# backup the config file
-sudo cp "network_manager_config_file" "network_manager_config_file.bak"
-
-# value to search for
-search_string="managed=false"
-
-# value to replace with
-replace_string="managed=true"
-
-# replace value with sed
-sed -i "s/$search_string/$replace_string/g" "network_manager_config_file"
-
 echo "Rebooting in 5 seconds"
 
 sleep 5
