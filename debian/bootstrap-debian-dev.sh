1a459d6459d9a84f880475255b267762405ed1dc
Moses E. Miles III <vamem9z@gmail.com>
2024-12-19 20:28:38 -0500

    Fix nala commands and add missing utilities

--- debian/bootstrap-debian-dev.sh
+++ debian/bootstrap-debian-dev.sh
@@ -2,11 +2,11 @@
 
 # Make sure we are up to date
 echo "Update repo"
-sudo apt -y update && sudo apt-y upgrade
+sudo apt update -y && sudo apt upgrade -y
 
 # Add Utilities
 echo "Install utilities"
-sudo apt -y install nala zsh git tmux vim python-is-python3 gnome-keyring wget gpg fontconfig openssl software-properties-common gettext make cmake
+sudo apt install -y nala zsh git tmux vim python-is-python3 gnome-keyring wget gpg fontconfig openssl software-properties-common gettext make cmake htop sqlitebrowser
 
 # Ensure we are using the fastest mirror
 sudo nala fetch
@@ -17,9 +17,9 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >pac
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null
 rm -f packages.microsoft.gpg
-sudo nala -y install apt-transport-https
+sudo nala install -y apt-transport-https
 sudo nala update
-sudo nala -y install code # or code-insiders
+sudo nala install -y code # or code-insiders
 
 ## Add dependencies to build neovim from source
 echo "Add neovim build runtime dependencies"
@@ -27,12 +27,12 @@ sudo nala install -y gettext make cmake fzf wl-clipboard xclip
 
 ## Add dependencies to build ruby
 echo "Add ruby build dependencies"
-sudo nala -y install autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
+sudo nala install -y autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
 
 ## Add dependencies to build python
 echo "Add python build dependencies"
-sudo nala -y install build-essential zlib1g-dev libbz2-dev libssl-dev libreadline-dev libncurses5-dev libsqlite3-dev libgdbm-dev libdb-dev libexpat-dev libpcap-dev liblzma-dev libpcre3-dev libffi-dev
+sudo nala install -y build-essential zlib1g-dev libbz2-dev libssl-dev libreadline-dev libncurses5-dev libsqlite3-dev libgdbm-dev libdb-dev libexpat-dev libpcap-dev liblzma-dev libpcre3-dev libffi-dev
 
 ## Add dependencies for clojure
 echo "Add clojure dependencies"
-sudo nala -y install rlwrap
+sudo nala install -y rlwrap
--- debian/bootstrap-debian-gnome-minimal.sh
+++ debian/bootstrap-debian-gnome-minimal.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
 echo "Install nala, a better apt frontend"
-sudo apt -y install nala dialog
+sudo apt install -y nala dialog
 
 echo "Get the fastest mirror"
 sudo nala fetch
 
 echo "Install base gnome environment"
-sudo nala -y install gnome-shell gnome-terminal gnome-text-editor firefox-esr: nautilus nautilus-extension-gnome-terminal wl-clipboard xclip ffmpegthumbnailer dconf-editor
+sudo nala install -y gnome-shell gnome-terminal gnome-text-editor firefox-esr: nautilus nautilus-extension-gnome-terminal wl-clipboard xclip ffmpegthumbnailer dconf-editor
 
 echo "Install lightdm instead of gdm3 because of a bug with UTM on mac"
-sudo nala -y install lightdm
+sudo nala install -y lightdm
 
 echo "Setup graphical boot"
 sudo systemctl enable --now lightdm && sudo systemctl set-default graphical.target
