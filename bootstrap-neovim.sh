28f7264a3e9bb04bcbcee65bf261756276fb4a41
Moses E. Miles III <vamem9z@gmail.com>
2024-12-23 08:13:07 -0500

    Move nvim setup to nvim bootstrap
    Keeping the relevant concerns toogther

--- bootstrap-neovim.sh
+++ bootstrap-neovim.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+echo "Install neovim"
+
 mkdir -p $HOME/installed $HOME/Downloads
 cd $HOME/Downloads
 wget https://github.com/neovim/neovim/archive/refs/tags/stable.tar.gz 
@@ -10,6 +12,9 @@ cd $HOME/installed/neovim-stable
 rm -r build/  # clear the CMake cache
 make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
 make install
+
+echo "Restore neovim configuration"
+ln -s $PWD/nvim $HOME/.config/nvim 
 
 echo "Keep the installed directory to uninstall neovim"
 echo "Run 'cmake --build build/ --target uninstall' to uninstall"
