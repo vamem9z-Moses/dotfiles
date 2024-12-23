6a4b4793f4cd3845005031ba85f762fb68191815
Moses E. Miles III <vamem9z@gmail.com>
2024-12-22 22:24:36 -0500

    Allow conform to set formatter for erb / eruby files
    Disable LazyVim default config in favor of htmlbeautifier for erb /
    eruby formatting

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      eruby = { "htmlbeautifier" },
+      erb = { "htmlbeautifier" },
+      html = { "htmlbeautifier" },
+    },
+  },
+}
