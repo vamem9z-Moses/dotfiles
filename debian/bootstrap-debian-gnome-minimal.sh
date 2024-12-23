9833530d3cb23a44544648a70b6a0237ec3efef2
Moses E. Miles III <vamem9z@gmail.com>
2024-12-22 20:19:31 -0500

    Make graphic boot optional
    For my Mac UTM setup, I plan to login remotely to the vm and use tools
    like nvim and lazy git. I only need gnome when I am running something
    like dbeaver or sqbrowser to explore the database.

--- debian/bootstrap-debian-gnome-minimal.sh
+++ debian/bootstrap-debian-gnome-minimal.sh
@@ -7,15 +7,12 @@ echo "Get the fastest mirror"
 sudo nala fetch
 
 echo "Install base gnome environment"
-sudo nala install -y gnome-shell gnome-terminal gnome-text-editor firefox-esr: nautilus nautilus-extension-gnome-terminal wl-clipboard xclip ffmpegthumbnailer dconf-editor
+sudo nala install -y gnome-shell gnome-terminal gnome-text-editor firefox-esr nautilus nautilus-extension-gnome-terminal wl-clipboard xclip ffmpegthumbnailer dconf-editor
 
 echo "Install lightdm instead of gdm3 because of a bug with UTM on mac"
 sudo nala install -y lightdm
 
-echo "Setup graphical boot"
-sudo systemctl enable --now lightdm && sudo systemctl set-default graphical.target
-
-echo "Rebooting in 5 seconds"
-
-sleep 5
-sudo reboot
+echo "Setup graphical boot -- optional"
+echo "To set up graphical boot run the following"
+echo "sudo systemctl enable --now lightdm && sudo systemctl set-default graphical.target"
+echo "Reboot after the command completes"
