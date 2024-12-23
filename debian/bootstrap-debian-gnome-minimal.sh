b13c8cb201a76ecc91f8190dfffdaf614e6b7f62
Moses E. Miles III <vamem9z@gmail.com>
2024-12-23 09:21:19 -0500

    Add system util apps
    Add basic system utility apps to make the mininal environment easier to
    use

--- debian/bootstrap-debian-gnome-minimal.sh
+++ debian/bootstrap-debian-gnome-minimal.sh
@@ -7,7 +7,10 @@ echo "Get the fastest mirror"
 sudo nala fetch
 
 echo "Install base gnome environment"
-sudo nala install -y gnome-shell gnome-terminal gnome-text-editor firefox-esr nautilus nautilus-extension-gnome-terminal wl-clipboard xclip ffmpegthumbnailer dconf-editor
+sudo nala install -y gnome-shell tilix gnome-terminal gnome-text-editor firefox-esr nautilus nautilus-extension-gnome-terminal wl-clipboard xclip ffmpegthumbnailer
+
+echo "Install gnome utils"
+sudo nala install -y gnome-system-log gnome-system-monitor gnome-usage gnome-system-tools gnome-tweaks dconf-editor
 
 echo "Install lightdm instead of gdm3 because of a bug with UTM on mac"
 sudo nala install -y lightdm
