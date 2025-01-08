300087036480ced196f777201ebde6c449e0fd55
Moses E. Miles III <vamem9z@gmail.com>
2025-01-08 14:14:20 -0500

    Refactor Lazy Git bootstrap

--- bootstrap-lazygit.sh
+++ bootstrap-lazygit.sh
@@ -2,32 +2,50 @@
 
 go_version = ""
 
-function install_lazygit() {
-  if [[ -e $HOME/.local/bin/mise ]]; then
-    mise install "go@$go_version"
-    mise use -g "go@$go_version"
-    mkdir -p $HOME/installed
-    cd $HOME/installed
-    git clone https://github.com/jesseduffield/lazygit.git
-    cd lazygit
-    go install
-    echo "LazyGit is installed and ready to use to use it enter lazygit inside a git repository"
+go_install_intro='LazyGit is installed from source and requires an up to date version of go.
+Mise is used to install and manage the version number and will install a globally available version of go.'
+
+re='^[0-9]+(\.[0-9]+)*$'
+
+install_dir = $HOME/installed/
+
+function get_go_ver() {
+  if [[ $go_version =~ $re ]]; then
+    echo $go_install_intro
+    echo " "
+    read -p 'Enter Golang Version Number: ' go_version
   else
-    echo "Mise is missing from the PATH. Please install mise or add it to the path."
+    echo "'$version' must be a valid version number"
+    exit 1
   fi
 }
 
-intro='LazyGit is installed from source and requires an up to date version of go.
-Mise is used to install and manage the version number and will install a globally available version of go.'
+function setup_install_dirs() {
+  mkdir -p install_dir
+  cd install_dir
+}
 
-echo "$intro"
-echo " "
+function install_go() {
+  if [[ -e $HOME/.local/bin/mise ]]; then
+    mise install "go@$go_version"
+    mise use -g "go@$go_version"
+  else
+    echo "Mise is missing from the PATH. Please install mise or add it to the path."
+    exit 1
+  fi
+}
 
-read -p 'Enter Golang Version Number: ' go_version
+function install_lazygit() {
+  git clone https://github.com/jesseduffield/lazygit.git
+  cd lazygit
+  go install
+  echo "LazyGit is installed and ready to use to use it enter lazygit inside a git repository"
+}
 
-re='^[0-9]+(\.[0-9]+)*$'
-if [[ $go_version =~ $re ]]; then
-  install_lazygit
-else
-  echo "'$version' must be a valid version number"
+if ! command -v go 2>&1 >/dev/null; then
+  get_go_ver
+  install_go
 fi
+
+setup_install_dirs
+install_lazygit
