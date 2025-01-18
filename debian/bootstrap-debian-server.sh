5dad9ee29b913433ab9dfd54549732d465a3f98d
Moses E. Miles III <vamem9z@gmail.com>
2025-01-18 11:18:22 -0500

    Remove Powertop
    Powertop makes emby run to slowly especially when processing libraries

--- debian/bootstrap-debian-server.sh
+++ debian/bootstrap-debian-server.sh
@@ -1,13 +1,10 @@
-!# /bin/sh
+#!/bin/sh
 
 echo "Update apt and upgrade"
 sudo apt update && sudo apt upgrade -y
 
 echo "Install basic tools"
-sudo apt install -y curl wget htop vim smartmontools lm-sensors powertop firmware-linux git
+sudo apt install -y curl wget htop vim smartmontools lm-sensors firmware-linux git
 
 echo "Run Sensors Detect"
 sudo sensors-detect
-
-echo "Setup Powertop"
-sudo system enable --now powertop
