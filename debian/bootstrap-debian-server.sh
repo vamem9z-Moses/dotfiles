49fb4a962a66065dd0376f89f21028a1c08ffd3f
Moses E. Miles III <vamem9z@gmail.com>
2025-01-18 11:19:19 -0500

    Add additional tools to basic server config

--- debian/bootstrap-debian-server.sh
+++ debian/bootstrap-debian-server.sh
@@ -4,7 +4,7 @@ echo "Update apt and upgrade"
 sudo apt update && sudo apt upgrade -y
 
 echo "Install basic tools"
-sudo apt install -y curl wget htop vim smartmontools lm-sensors firmware-linux git
+sudo apt install -y curl wget htop vim smartmontools lm-sensors firmware-linux git tree lshw
 
 echo "Run Sensors Detect"
 sudo sensors-detect
