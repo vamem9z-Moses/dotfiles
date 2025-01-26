ad5b557b481e63e836ab6fef3341bc47db0df020
Moses E. Miles III <vamem9z@gmail.com>
2025-01-25 20:33:36 -0500

    Add rsync and powertop to basic bootstrap

--- debian/bootstrap-debian-server.sh
+++ debian/bootstrap-debian-server.sh
@@ -4,7 +4,7 @@ echo "Update apt and upgrade"
 sudo apt update && sudo apt upgrade -y
 
 echo "Install basic tools"
-sudo apt install -y curl wget htop vim smartmontools lm-sensors firmware-linux git tree lshw
+sudo apt install -y rsync curl wget htop vim smartmontools lm-sensors firmware-linux git tree lshw powertop
 
 echo "Run Sensors Detect"
 sudo sensors-detect
