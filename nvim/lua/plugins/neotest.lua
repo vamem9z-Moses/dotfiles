03d500e8794dcf4d042ea321ef3b31ad23a66ecc
Moses E. Miles III <vamem9z@gmail.com>
2025-09-09 10:46:08 -0400

    Update neovim to latest working version of neotest with ruby

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,18 +1,13 @@
 return {
-  "nvim-neotest/neotest",
-  lazy = true,
-  dependencies = {
-    "zidhuss/neotest-minitest",
+  {
     "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
     "antoinemadec/FixCursorHold.nvim",
     "nvim-treesitter/nvim-treesitter",
+    "zidhuss/neotest-minitest",
   },
-  config = function()
-    require("neotest").setup({
-      adapters = {
-        require("neotest-minitest"),
-      },
-    })
-  end,
+  {
+    "nvim-neotest/neotest",
+    opts = { adapters = { "neotest-minitest" } },
+  },
 }
